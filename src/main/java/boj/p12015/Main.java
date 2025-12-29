9b1d8ad8a0a55db728f85e1b227fe9597f709520
parksy2211 <parksy20022@gmail.com>
2025-12-29 23:39:56 +0900

    25.12.29

---
+++ src/main/java/boj/p12015/Main.java
@@ -0,0 +1,16 @@
+package boj.p12015;
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+
+        for(int i=0;i<N;i++){
+
+        }
+    }
+}
---
+++ src/main/java/boj/p1300/Main.java
@@ -0,0 +1,28 @@
+package boj.p1300;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 예시 입력 (한 줄 정수)
+        int N = Integer.parseInt(br.readLine());
+        int k = Integer.parseInt(br.readLine());
+//        long[] arr = new long[N*N];
+//        long idx=0;
+//        for(int i=1;i<=N;i++) {
+//            for(int j=1;j<=N;j++) {
+//                arr[(int) idx++] = i*j;
+//            }
+//        }
+//
+//        Arrays.sort(arr);
+//        System.out.print(arr[k-1]);
+
+
+        //
+
+    }
+}
